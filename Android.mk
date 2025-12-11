792fdf38370b105f01885d5a27b0679538c8cbb6
azure <furkivideoeditor@gmail.com>
2025-12-11 18:51:14 +0300

    G501: Add SoftGatekeeper & Vulkan symlinks

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),v7101o)
+ifeq ($(TARGET_DEVICE),G501)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
@@ -19,30 +19,6 @@ $(GATEKEEPER_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf libSoftGatekeeper.so $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_SYMLINK)
-
-GATEKEEPER_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.mt6771.so
-GATEKEEPER_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.mt6771.so
-$(GATEKEEPER_TRUSTKERNEL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf gatekeeper.trustkernel.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(GATEKEEPER_TRUSTKERNEL_SYMLINK)
-
-KEYSTORE_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/keystore.mt6771.so
-KEYSTORE_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/keystore.mt6771.so
-$(KEYSTORE_TRUSTKERNEL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf keystore.v2.trustkernel.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KEYSTORE_TRUSTKERNEL_SYMLINK)
-
-KMSETKEY_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/kmsetkey.mt6771.so
-KMSETKEY_TRUSTKERNEL_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/kmsetkey.mt6771.so
-$(KMSETKEY_TRUSTKERNEL_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@mkdir -p $(dir $@)
-	$(hide) ln -sf kmsetkey.trustkernel.so $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(KMSETKEY_TRUSTKERNEL_SYMLINK)
 
 VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib/hw/vulkan.mt6771.so
 VULKAN_SYMLINK += $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.mt6771.so
